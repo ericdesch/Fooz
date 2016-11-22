9a2096825676881e8be63acad476140ff203842d
ericdesch <ericdesch@outlook.com>
2016-11-21 20:09:47 -0800

    Initial commit

---
+++ Fooz.Caching/ICacheProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+
+namespace Fooz.Caching
+{
+    public interface ICacheProvider
+    {
+        object Get(string key);
+        void Set(string key, object data, CacheItemPolicy policy);
+        void Set(string key, object data, int cacheTime);
+        bool IsSet(string key);
+        void Invalidate(string key);
+    }
+}
